5178c9ef282a84a3ee552c5887037739be18c2ac
Jonny <jonny.covert@gmail.com>
2016-07-26 15:09:07 -0400

    New VAGRANT_HOME

--- fish/config.fish
+++ fish/config.fish
@@ -11,4 +11,5 @@ switch (uname)
     set -U fish_user_paths /home/jonny/bin /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl ~/.cargo/bin/
   case Darwin
     set -U fish_user_paths /usr/local/opt/llvm/bin /usr/local/bin ~/.cargo/bin/
+    set -U VAGRANT_HOME /Volumes/External/.vagrant.d/
 end
