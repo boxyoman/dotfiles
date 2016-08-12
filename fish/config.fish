dd5a6691bf1835d6d8d9af24055db31e75c627bb
Jonny <jonny.covert@gmail.com>
2016-08-12 02:19:10 -0400

    Added stack stuff of ghc.

--- fish/config.fish
+++ fish/config.fish
@@ -8,8 +8,8 @@ set -Ux EDITOR nvim
 
 switch (uname)
   case Linux
-    set -U fish_user_paths /home/jonny/bin /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl ~/.cargo/bin/
+    set -U fish_user_paths /home/jonny/bin /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl ~/.cargo/bin/ ~/.local/bin/ 
   case Darwin
-    set -U fish_user_paths /usr/local/opt/llvm/bin /usr/local/bin ~/.cargo/bin/
+    set -U fish_user_paths /usr/local/opt/llvm/bin /usr/local/bin ~/.cargo/bin/ ~/.local/bin/ 
     set -U VAGRANT_HOME /Volumes/External/.vagrant.d/
 end
