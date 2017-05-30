d69ff1862dead451ef0cec362c03fb14e40a79ed
Jonny <jonny.covert@gmail.com>
2017-05-30 17:26:28 -0400

    Stuffs.

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,22 @@ alias timeout gtimeout
 
 setenv EDITOR nvim
 
+function add-to-path -d "Add directories to \$PATH"
+  for dir in $argv
+    if begin
+        test -d $dir
+        and not contains $dir $PATH
+      end
+      set -x PATH $dir $PATH
+    end
+  end
+end
+
 switch (uname)
   case Linux
     set -U fish_user_paths /home/jonny/bin /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl ~/.cargo/bin/ ~/.local/bin/
   case Darwin
-    set -U fish_user_paths /usr/local/bin ~/.cargo/bin/ ~/.local/bin/ ~/.nix-profile/bin
+    set -U fish_user_paths  /usr/local/bin ~/.cargo/bin/ ~/.local/bin/ /Library/Frameworks/Mono.framework/Commands
     set -U VAGRANT_HOME /Volumes/External/.vagrant.d/
     source ~/.nix-profile/etc/profile.d/nix.fish
 end
