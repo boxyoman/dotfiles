7df1f022bb8a20e197d4a1f0ea686d5dbd74a1c9
Jonny <jonny.covert@gmail.com>
2017-06-24 20:50:40 -0400

    Added ghcid.

--- nvim/nvim.hs
+++ nvim/nvim.hs
@@ -1,9 +1,13 @@
 import Neovim
 
 import qualified Neovim.Example.Plugin as Example
+import qualified Neovim.Ghcid as Ghcid
 
 main :: IO ()
 main = do
     neovim defaultConfig
-        { plugins = plugins defaultConfig ++ [ Example.plugin ]
+        { plugins = plugins defaultConfig ++
+          [ Example.plugin
+          , Ghcid.plugin
+          ]
         }
